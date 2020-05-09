4e433fb9bb86dcdad5bdba81562a1817d8a5423a
Ufuk <u.angay@gmail.com>
2020-05-09 21:09:53 +0200

    Started working with ship-WIP

---
+++ ContainerApp/Ship.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContainerApp
+{
+    class Ship
+    {
+        public List<Place> Places { get; }
+        public double MaxWeight { get; }
+        public double MinWeight { get; }
+        
+        public Ship(List<Place> places, double maxWeight)
+        {
+            Places = places;
+            MaxWeight = maxWeight;
+            MinWeight = CalculateMinWeight();
+        }
+
+        //Bereken het minimum gewicht van het schip.
+        private double CalculateMinWeight()
+        {
+            return Convert.ToDouble(MaxWeight / 2);
+        }
+        
+        //To-Do's
+
+        //Balanceren van het schip om kapseizen te voorkomen.
+        //Berekent de balans tussen rij 1 en rij 2.
+    }
+}
